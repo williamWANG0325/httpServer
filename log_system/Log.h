f90c1a4177e71b36785a611e4395d433a755b94b
williamWANG0325 <979084343@qq.com>
2022-02-20 01:07:51 +0800

    Create Log.h

---
+++ log_system/Log.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "BlockQueue.h"
+#include <thread>
+#include <string>
+#include <atomic>
+#include <mutex>
+#include <cstdarg>
+#include <string.h>
+#include <time.h>
+
+class Log
+{
+public:
+    static Log* getInstence();
+    void openLog(int max_buffer_size = 1024 );//线程不安全
+    void closeLog();//线程不安全
+    void writeLog(int level, const char* format, ...);
+    ~Log();
+
+private:
+    Log();
+    void writeLogToFile();
+
+private:
+    std::atomic_bool alive;
+
+    FILE* filePtr;
+    char logName[256];
+
+//    int maxLine;
+//    long long countLine;
+
+    BlockQueue<std::string> logQueue;
+    
+    char* buffer;
+    int maxBufferSize;
+    std::mutex mutexLock;
+
+    std::atomic_int today;
+    std::thread writer;
+
+
+};
+
