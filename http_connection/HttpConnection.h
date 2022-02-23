1c2099f5a499a818df51f7706afa239aa8f238da
williamWANG0325 <979084343@qq.com>
2022-02-23 23:17:09 +0800

    Update HttpConnection.h

--- http_connection/HttpConnection.h
+++ http_connection/HttpConnection.h
@@ -7,7 +7,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/epoll.h>
-#include <string.h>
+#include <cstring>
+#include <cstdarg>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
 
 class HttpConnection
 {
@@ -21,8 +25,7 @@ public:
         GET,
         POST
     };
-    enum HTTP_CODE
-    {
+    enum HTTP_CODE{
         NO_REQUEST,
         GET_REQUEST,
         BAD_REQUEST,
@@ -32,6 +35,11 @@ public:
         INTERNAL_ERROR,
         CLOSED_CONNECTION
     };
+    enum CHECK_STATE{
+        CHECK_STATE_REQUESTLINE,
+        CHECK_STATE_HEADER,
+        CHECK_STATE_CONTENT
+    };
 public:
     static const int FILENAME_LEN = 200;
     static const int READ_BUFFER_SIZE = 2048;
@@ -39,19 +47,36 @@ public:
 
     static std::atomic_int totalConnection;
     static std::atomic_int epollFd;
+    static const char* rootPath;
+    static std::atomic_bool isET;
+
 
 public:
     HttpConnection() ;
     ~HttpConnection() ;
 
     void init(int epollFd_, int sockFd, const sockaddr_in & addr);
+    void init();
     void closeConnection();
     
     LINE_STATUS parseLine();
     bool readToBuffer();
     
-    HTTP_CODE parseRequestLine(char *test);
+    HTTP_CODE parseRequestLine(char *text);
+    HTTP_CODE parseHeaders(char* text);
+    HTTP_CODE parseContent(char* text);
+    HTTP_CODE parse();
 
+    bool write();
+    HTTP_CODE doRequest();
+    bool fillWriteBuffer(HTTP_CODE ret);
+
+    bool addResponse(const char* format, ...) ;
+    bool addStatusLine(int status, const char* title);
+    bool addContentType(char* s = nullptr) ;
+    bool addHeaders(int responseLen);
+
+    void unmap();
 
 private:
     int fd;
@@ -65,15 +90,30 @@ private:
     int parsedIdx;
 
 
-
     char writeBuffer[WRITE_BUFFER_SIZE];
+    int writeIdx;
+    int bytesHaveSend;
+    int bytesToSend;
+
+    struct iovec writeIV[2];
+    int ivCount;
+
+
+    char requestFile[FILENAME_LEN];
+
+    CHECK_STATE masterState;
 
     METHOD method;
+
     char* url;
     char* version;
     char* host;
+    char* content;
+    int contentLen;
+    bool keepAlive;
+    struct stat fileStat;
+    char * mmapWriteFile;
 
-    std::atomic_bool isET;
 
 };
 std::atomic_int HttpConnection::totalConnection = 1;
